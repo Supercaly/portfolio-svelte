16b4768491263a18423c82c3598e9b13adcaa873
Lorenzo Calisti <djonline96@gmail.com>
2024-10-24 14:17:03 +0200

    Add main layout and style
    This commit adds the layout file for the main route and the main style sheet.
    Also this commit installs the main fonts as depensencies.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import "../app.css";
+
+    let {children} = $props();
+</script>
+
+<div class="app">
+    <main>
+        {@render children()}
+    </main>
+</div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        /* TODO: Make margin top responsive */
+        margin-top: var(--100px);
+        padding: 1rem;
+        /* TODO: Make max width responsive */
+        max-width: var(--90px);
+        box-sizing: border-box;
+    }
+
+</style>
