95fdd707b1ec16d1a6b6fb6aaa733b2df20792e5
Lorenzo Calisti <djonline96@gmail.com>
2024-10-30 13:35:01 +0100

    Add experience page
    This commit adds the main content of the experience page. Also it adds the `ActivityCard.svelte` component.

---
+++ src/lib/components/ActivityCard.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    import BaseCard from "./BaseCard.svelte";
+    import MapPinIcon from "$lib/assets/icons/map-pin.svg?component";
+    import Icon from "./Icon.svelte";
+
+    interface Props {
+        start: string;
+        end?: string;
+        title: string;
+        institution: string;
+        description?: string;
+        location: string;
+    }
+
+    const { start, end, title, institution, description, location }: Props =
+        $props();
+</script>
+
+<BaseCard>
+    <div class="content">
+        <div class="period">
+            <span class="label-medium">{start}</span>
+            {#if end !== undefined}
+                <span class="label-medium">-</span>
+                <span class="label-medium">{end}</span>
+            {/if}
+        </div>
+        <div class="text-content">
+            <h5 class="title-large">{title}</h5>
+            <p class="body-medium">{institution}</p>
+            {#if description !== undefined}
+                <p class="body-small">{description}</p>
+            {/if}
+            <div class="location">
+                <Icon><MapPinIcon /></Icon>
+                <p class="body-small">{location}</p>
+            </div>
+        </div>
+    </div>
+</BaseCard>
+
+<style lang="scss">
+    @use "../../mixins.scss" as *;
+
+    .content {
+        display: grid;
+        grid-template-columns: 150px auto;
+        padding: var(--24px) var(--32px);
+
+        @include md($max: $screen-medium) {
+            display: flex;
+            flex-direction: column;
+            gap: var(--24px);
+            padding: var(--24px);
+        }
+
+        .period {
+            width: fit-content;
+            height: fit-content;
+            display: flex;
+            gap: var(--4px);
+            padding: var(--6px) var(--12px);
+            color: var(--color-on-primary);
+            background: var(--color-primary);
+            border-radius: var(--48px);
+        }
+
+        .location {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: var(--8px);
+        }
+
+        .text-content {
+            display: flex;
+            flex-direction: column;
+            gap: var(--10px);
+            color: var(--color-on-surface);
+        }
+    }
+</style>
---
+++ src/routes/experience/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import ActivityCard from "$lib/components/ActivityCard.svelte";
+	import SectionHeader from "$lib/components/SectionHeader.svelte";
+
+	const studies = [
+		{
+			start: "20XX",
+			end: "present",
+			title: "A Fake study 1.",
+			institution: "A place where we did something.",
+			description: "A long description of what we did.",
+			location: "Somewhere",
+		},
+		{
+			start: "20XX",
+			title: "A Fake study 2.",
+			institution: "A place where we did something.",
+			location: "Somewhere",
+		},
+		{
+			start: "20XX",
+			end: "20XX",
+			title: "A Fake study 3.",
+			institution: "A place where we did something.",
+			location: "Somewhere",
+		},
+	];
+	const works = [
+		{
+			start: "20XX",
+			end: "present",
+			title: "A Fake work 1.",
+			institution: "A place where we did something.",
+			description: "A long description of what we did.",
+			location: "Somewhere",
+		},
+		{
+			start: "20XX",
+			title: "A Fake work 2.",
+			institution: "A place where we did something.",
+			location: "Somewhere",
+		},
+	];
+</script>
+
+<svelte:head>
+	<title>Experience</title>
+	<meta
+		name="description"
+		content="Here I tell you my studies and my work experiences."
+	/>
+</svelte:head>
+
+<section>
+	<SectionHeader
+		title="Study."
+		description="A list of all my academic degrees."
+	/>
+	<div class="content">
+		{#each studies as study}
+			<ActivityCard {...study} />
+		{/each}
+	</div>
+</section>
+<section>
+	<SectionHeader
+		title="Work."
+		description="A list of jobs and internships done in the years."
+	/>
+	<div class="content">
+		{#each works as work}
+			<ActivityCard {...work} />
+		{/each}
+	</div>
+</section>
+
+<style lang="scss">
+	@use "../../mixins.scss" as *;
+
+	.content {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--24px);
+		padding: var(--spacing-l);
+
+		@include md($screen-large) {
+			gap: var(--32px);
+		}
+	}
+</style>
