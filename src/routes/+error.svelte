a6fca9a315658fbbe667bb7e0a807c3c29c02399
Lorenzo Calisti <djonline96@gmail.com>
2024-11-03 18:53:30 +0100

    Add error page
    This commit adds a default page displayed when there is an error or we are trying to access an unknown resource.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import SectionHeader from "$lib/components/SectionHeader.svelte";
+</script>
+
+<section>
+    <SectionHeader
+        title="Oops!"
+        description="It looks like you've reached an unknown destination. Try heading back!"
+    />
+    <a href="/" class="title-medium">Home</a>
+</section>
+
+<style lang="scss">
+    a {
+        text-decoration: none;
+        color: var(--color-on-primary);
+        background: var(--color-primary);
+        padding: var(--16px) var(--24px);
+        border-radius: var(--12px);
+    }
+</style>
