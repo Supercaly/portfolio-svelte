bc7698d5f2f3350c34207ae2ba111c0b34db84a3
Lorenzo Calisti <djonline96@gmail.com>
2024-11-06 20:53:12 +0100

    Change publications page to be sorted by year
    This commit changes the pubblications page to show the papers divided by year.

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import ArticleCard from "$lib/components/ArticleCard.svelte";
 	import SectionHeader from "$lib/components/SectionHeader.svelte";
+	import SubsectionHeader from "$lib/components/SubsectionHeader.svelte";
 	import publications from "$lib/config/publications.json";
 
 	const articles = publications["articles"];
@@ -57,8 +58,15 @@
 		</div>
 	</div>
 	<div class="content">
-		{#each articles as article}
-			<div class="project"><ArticleCard {...article} /></div>
+		{#each articles as year}
+			<div class="year">
+				<SubsectionHeader title={year.title} />
+				<div class="articles">
+					{#each year.articles as article}
+						<div class="project"><ArticleCard {...article} /></div>
+					{/each}
+				</div>
+			</div>
 		{/each}
 	</div>
 </section>
@@ -83,18 +91,32 @@
 
 	.content {
 		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(2, minmax(0, 1fr));
-		gap: var(--24px);
+		display: flex;
+		flex-direction: column;
 		padding: var(--spacing-l) 0px;
 
-		@include md($screen-large) {
-			grid-template-columns: repeat(4, minmax(0, 1fr));
-			gap: var(--32px);
-		}
+		.year {
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			
+			.articles {
+				width: 100%;
+				display: grid;
+				grid-template-columns: repeat(2, minmax(0, 1fr));
+				gap: var(--24px);
+				padding: var(--spacing-l) 0px;
 
-		.project {
-			grid-column: span 2 / span 2;
+				@include md($screen-large) {
+					grid-template-columns: repeat(4, minmax(0, 1fr));
+					gap: var(--32px);
+				}
+			}
+
+			.project {
+				grid-column: span 2 / span 2;
+			}
 		}
 	}
 </style>
